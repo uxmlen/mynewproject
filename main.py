30736f4a9a3b415696f8f620a00006674f2fd5ce
Your Name <you@example.com>
2024-02-28 12:43:49 +0300

    Add three numbers

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # main.py
-def add_two(a: int, b: int) -> int:
+def add_three(a: int, b: int, c: int) -> int:
     """sum two number"""
     return a + b
 
 
 def main() -> None:
-    """add two number"""
-    a, b = map(int, input().split())
-    res = add_two(a, b)
+    """add three number"""
+    print("enter three numbers: 3 3 3")
+    a, b, c = map(int, input().split())
+    res = add_three(a, b, c)
     print(res)
 
 
